47c00c1f292d889cc952888f11ba8c94685b6d4b
Phelan Faubert <phelanfaubert@gmail.com>
2022-03-09 03:11:35 +0000

    Add teachers_seeds.sql & assistance_requests_seeds.sql to seeds folder

--- 2_queries_joins/1_time_spent_all_assignments.sql
+++ 2_queries_joins/1_time_spent_all_assignments.sql
@@ -1,3 +1,4 @@
-SELECT count(duration)
-from assignment_seeds
-WHERE name IS 'Ibrahim Schimmel'
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
